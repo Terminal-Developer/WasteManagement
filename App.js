2263ccbed1aeb44cc4af477892047bae27effabe
SachetVijay <81145614+SachetVijay@users.noreply.github.com>
2022-01-08 13:03:39 +0530

    Update

--- App.js
+++ App.js
@@ -2,27 +2,23 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import UserTab from './src/pages/UserTab'
+import UserTab from './src/pages/UserTab';
 import Login from './src/pages/Login';
 
 export default function App() {
   return (
-    
-        
-
     <View style={styles.root}>
-      <UserTab/>
+      {/* <UserTab/> */}
       <Login />
     </View>
-
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
